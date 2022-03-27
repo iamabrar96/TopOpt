27445c3ae161cf2b577556d04169a9f3e5742447
Zevrap18 <parvezmushtaak@gmail.com>
2022-03-27 18:34:04 +0200

    added neuralnet optimizer model

--- common.py
+++ common.py
@@ -2,6 +2,7 @@ from platform import node
 import gmsh
 from matplotlib.pyplot import axis
 import numpy as np
+import torch
 
 from parameters import Parameters
 class GMSH_helper():
@@ -103,3 +104,15 @@ class Topology_viz:
         gmsh.option.setNumber('Mesh.VolumeEdges',0)
 
         gmsh.fltk.run()
+
+def device_common():
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+        print('device is assigned as CUDA')
+        torch.cuda.empty_cache()
+
+    else:
+        device = torch.device("cpu")   
+        print('device is assigned as CPU')
+        
+    return device
--- neuralnet_optimizer.py
+++ neuralnet_optimizer.py
@@ -1,81 +1,38 @@
 import numpy as np
-import pandas as pd
 import torch
 import torch.nn as nn
-import parameters
+from common import GMSH_helper, Topology_viz, device_common
 
 from sklearn.preprocessing import MinMaxScaler
+from parameters import Parameters
 
+from topopt import FE_solver
 
-class Dataset():
-    def __init__(self):
-        super(Dataset,self).__init__()
+class NN_Optimizer:
+    def __init__(self, params) -> None:
+        self.params = params
+        self.solver= FE_solver(params)
+        self.helper= GMSH_helper(params)
+        self.topology_visualizer= Topology_viz(params)
 
-        # read and define inputs
-        self.data_name=pd.read_csv('Experiments_1_without_Error_Term.csv', sep=';', decimal=',') # small dataset
-        #self.data_name=pd.read_csv('Experiments_1_without_Error_Term_2.csv', sep=';', decimal=',') # large dataset
-        self.x = self.data_name[['F_press','fs','h_BT']].values
-        
-        # normilize inputs between -1 and 1
-        scaler_x = MinMaxScaler(feature_range=(-1,1))
-        self.x = scaler_x.fit_transform(self.x)
-        self.x = torch.tensor(self.x, dtype=torch.float64)
-        
-        print(f'size input: {self.x.size()}, min input: {self.x.min()}, max input: {self.x.max()}')
+        self.device= device_common()
+        self.model= TopOpt_NN(params).to(self.device)
+        self.inputs= torch.from_numpy(self.helper.get_element_centers()).to(self.device)
 
-        
-        # read and define outputs
-        disp_all = np.load("disp_all_81000001.npy") # small dataset
-        #disp_all = np.load("disp_all_81000001_2.npy") # large dataset
-        self.y = np.reshape(disp_all, (disp_all.shape[0],-1))
-        
-        # normalize displacement results between -1 and 1
-        scaler_y = MinMaxScaler(feature_range=(-1,1))
-        self.y = scaler_y.fit_transform(self.y)
-        self.y = torch.tensor(self.y, dtype=torch.float64)        
-        
-        print(f'size output: {self.y.size()}, min output: {self.y.min()}, max output: {self.y.max()}')   
+        self.criterion= loss_fn
+    def train(self):
+        train_loss = []
+        valid_loss = []
 
-        self.n_samples = self.x.shape[0]
-        print('size dataset:', self.n_samples)
-        
-    def __getitem__(self, index):
-        return self.x[index],self.y[index]  
-    
-    def __len__(self):
-        return self.n_samples
+        for epoch in range(epochs):
+            model.train()
+            batch_loss = []
 
-# class Model(nn.Module):
-#     def __init__(self, in_size, s, outsize):
-#         super(Model,self). __init__()
-
-#         self.l1 = nn.Linear(in_size, s, bias=True)
-#         self.relu = nn.ReLU()
-#         self.l2 = nn.Linear(s, outsize, bias=True)
-
-#     def forward(self, x):
-#         out = self.l1(x)
-#         out = self.relu(out)
-#         out = self.l2(out)
-#         return out
-
-def train(model, train_loader, val_loader, optimizer, epochs, device, criterion):
-    model.to(device)
-    train_loss = []
-    valid_loss = []
-    n_total_steps = len(train_loader)
-    
-    for epoch in range(epochs):
-        model.train()
-        batch_loss = []
-        for i,(x,y) in enumerate(train_loader):
             optimizer.zero_grad()
 
-            x = x.to(device)
-            y = y.to(device)
-            yhat = model(x.float())
+            density_hat = model(self.inputs)
 
-            loss = criterion(y,yhat)
+            loss = self.criterion(density,density_hat)
 
             loss.backward()
             optimizer.step()
@@ -85,51 +42,38 @@ def train(model, train_loader, val_loader, optimizer, epochs, device, criterion)
             if (i+1) % 1 == 0:
                 print (f'Epoch [{epoch+1}/{epochs}], Step [{i+1}/{n_total_steps}], Train-Step-Loss: {torch.log10(loss.item()):.4f}')
 
-        train_loss.append(torch.Tensor(batch_loss).mean().item())
+            train_loss.append(torch.Tensor(batch_loss).mean().item())
 
-        with torch.no_grad():
-            model.eval()
-            batch_loss = []
-            
-            for x,y in val_loader:
+            with torch.no_grad():
+                model.eval()
+                batch_loss = []
                 
-                x = x.to(device)
-                y = y.to(device)
-                yhat = model(x.float())
-                
-                loss = criterion(y,yhat)
-                
-                batch_loss.append(torch.log10(loss).item())
-                
-            valid_loss.append(torch.Tensor(batch_loss).mean().item()) 
-        print(f'Epoch [{epoch+1}/{epochs}], Train-Loss: {train_loss[epoch]:.4f}, Valid-Loss: {valid_loss[epoch]:.4f}')
-    return train_loss, valid_loss
+                for self.inputs,density in val_loader:
+                    
+                    self.inputs = self.inputs.to(device)
+                    density = density.to(device)
+                    density_hat = model(self.inputs.float())
+                    
+                    loss = self.criterion(density,density_hat)
+                    
+                    batch_loss.append(torch.log10(loss).item())
+                    
+                valid_loss.append(torch.Tensor(batch_loss).mean().item()) 
+            print(f'Epoch [{epoch+1}/{epochs}], Train-Loss: {train_loss[epoch]:.4f}, Valid-Loss: {valid_loss[epoch]:.4f}')
+        return train_loss, valid_loss
 
 
 
-def device_common():
-    if torch.cuda.is_available():
-        device = torch.device("cuda:0")
-        print('device is assigned as CUDA')
-        torch.cuda.empty_cache()
 
-    else:
-        device = torch.device("cpu")   
-        print('device is assigned as CPU')
-    
-    torch.cuda.device_count()
-    return device
 
-random.seed(manualSeed)
-
-class Model(nn.Module):
-    def __init__(self, in_size, n_hidden,size_hidden,outsize, p=0):
+class TopOpt_NN(nn.Module):
+    def __init__(self, params:Parameters):
         
         super(My_Model,self). __init__()
         self.in_size = in_size
         self.outsize = outsize
         self.n_hidden = n_hidden
-        self.size_hidden = size_hideen
+        self.size_hidden = size_hidden
         self.hidden=nn.ModuleList()
         self.drop=nn.Dropout(p=p)
         self.Layers = [in_size, *[self.size_hidden for i in range(n_hidden)], outsize]
@@ -151,7 +95,7 @@ class Model(nn.Module):
         return x
 
 
-def loss_top():
+def loss_fn():
     loss = torch.sum(torch.div(Jelem,nn_rho**penal))/obj0; # compliance
     volConstraint =((torch.mean(nn_rho)/desiredVolumeFraction) - 1.0);
     return loss, volConstraint
